cb7df0875942dc2682091c9a7ed4ebe81d00e256
Ben Tyler <ben.tyler@lrewater.com>
2020-02-20 18:10:01 -0700

    Stub out structure and develop example site

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { Suspense } from "react";
+import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import NotFound from "./components/NotFound";
+import Loading from "./components/Loading";
+import theme from "./theme";
 
-import ExampleComponent from 'lre-react'
+const Home = React.lazy(() => import("./pages/Home"));
 
-export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
-  }
-}
+const App = () => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Suspense fallback={<Loading />}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
+    </MuiThemeProvider>
+  );
+};
+
+export default App;
---
+++ example/src/theme.js
@@ -0,0 +1,20 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { indigo, green } from "@material-ui/core/colors";
+
+// This allows us to wrap the entire application in our custom theme
+export default createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: {
+      light: green[300],
+      main: green[600],
+      dark: green[700]
+    },
+    background: {
+      default: "#f1f1f1"
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
